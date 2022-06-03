f67b6153ac2cc5b7e4b3aff6b4d338cd0b800727
Andrea Sarmiento <anbesamo@gmail.com>
2022-06-03 13:56:55 -0400

    prueba

--- incisoB/vm.sh
+++ incisoB/vm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build ./subscriber -t image-subscriber:1.0.0
 docker build ./publisher -t image-publisher:1.0.0
-#docker swarm init
+# docker swarm init
+docker stack deploy -c docker-compose.yml ds-swarm-libros
