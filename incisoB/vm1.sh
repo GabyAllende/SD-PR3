98ed27a0640641680e8460d2badc11556780cc40
Andrea Sarmiento <anbesamo@gmail.com>
2022-06-03 18:50:18 -0400

    cambios 2

---
+++ incisoB/vm1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker build ./subscriber -t image-subscriber:1.0.0
+docker build ./publisher -t image-publisher:1.0.0
+
+docker swarm init
+docker stack deploy --compose-file docker-compose.yml ds-swarm-libros
--- incisoB/vm2.sh
+++ incisoB/vm2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build ./subscriber -t image-subscriber:1.0.0
 docker build ./publisher -t image-publisher:1.0.0
-docker swarm init
-docker stack deploy --compose-file docker-compose.yml ds-swarm-libros
+
+##comando join cuando este del swarm , o vm1 
